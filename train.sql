34134cfab64387d3de995befb43dd3275567411f
MAhilaThirumurugan <139051930+MAhilaThirumurugan@users.noreply.github.com>
2024-01-30 16:08:28 +0530

    Add files via upload

---
+++ train.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 30, 2024 at 11:35 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahiladb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `train`
+--
+
+CREATE TABLE `train` (
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `train`
+--
+
+INSERT INTO `train` (`name`, `email`, `password`) VALUES
+('mahila', 'mahil@gmail.com', 'mahila'),
+('mahila', 'mahi@gmail.com', '123456'),
+('Keerthika', 'Keerthika@gmail.com', 'keerthi'),
+('keerthi', 'keerthika@gmail.com', 'keerthi'),
+('dhanu', 'dhanu@gm', ''),
+('dhanu', 'dhanu@gmail.com', 'dhanu');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
